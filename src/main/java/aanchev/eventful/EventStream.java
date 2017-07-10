14f5a7f4b00b0d6fec3b9899b1595f4719a7deea
Anko Anchev <anko.anchev@gmail.com>
2017-07-10 17:13:15 +0300

    FAIL: Organizing Event Stream

--- src/main/java/aanchev/eventful/EventStream.java
+++ src/main/java/aanchev/eventful/EventStream.java
@@ -1,8 +1,12 @@
 package aanchev.eventful;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 //TODO tests
@@ -11,36 +15,36 @@ public interface EventStream<E> {
 
 	/* State Getters */
 	// may need overriding
-	
-	public Set<Handler<E>> getHandlers();
 
-	
+	public Collection<Handler<E>> getHandlers();
+
+
 	/* Handler/Listener Attaching */
-	
+
 	@SuppressWarnings("unchecked")
 	public default Handler<? extends E> on(Handler<? extends E> handler) {
 		getHandlers().add((Handler<E>) handler);
 		return handler;
 	}
-	
+
 	public default boolean off(Handler<? extends E> handler) {
 		return getHandlers().remove(handler);
 	}
 
-	
+
 	/* Firing of Events */
-	
+
 	public default boolean fire(E data) {
-		Set<Handler<E>> handlers = getHandlers();
-		
+		Iterable<Handler<E>> handlers = getHandlers();
+
 		if (handlers == null)
 			return true;
-		
+
 		try {
 			for (Handler<?> handler : handlers) {
 				handler.tryHandle(data);
 			}
-			
+
 			return true;
 		}
 		catch (VetoEventException e) {
@@ -50,28 +54,80 @@ public interface EventStream<E> {
 			return true;
 		}
 	}
-	
-	
+
+
 	/* Specialized Implementations */
-	
+
 	public interface Default<E> extends EventStream<E> {
-		final static Map<Object, Set<Handler<Object>>> eventStreams = new HashMap<>();
-		
+		final static Map<Object, Collection<Handler<Object>>> eventStreams = new HashMap<>();
+
 		@SuppressWarnings({ "unchecked", "rawtypes" })
 		@Override
-		public default Set<Handler<E>> getHandlers() {
-			return (Set<Handler<E>>) (Set) eventStreams.computeIfAbsent(this, k -> new HashSet<>());
+		public default Collection<Handler<E>> getHandlers() {
+			return (Set) eventStreams.computeIfAbsent(this, k -> new HashSet<>());
+		}
+
+
+		@Deprecated
+		public interface Organizing<E> extends Default<E>, EventStream.Organizing<E> {
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			@Override
+			public default Queue<Handler<E>> getHandlers() {
+				return (Queue) eventStreams.computeIfAbsent(this, k -> new LinkedList<>());
+			}
 		}
 	}
-	
+
+	@Deprecated
+	public interface Organizing<E> extends EventStream<E> {
+		@Override
+		default boolean fire(E data) {
+			Queue<Handler<E>> handlers;
+
+			try {
+				handlers = (Queue<Handler<E>>) getHandlers();
+			}
+			catch (ClassCastException e) {
+				return EventStream.super.fire(data);
+			}
+
+			if (handlers == null)
+				return true;
+
+			Collection<Handler<E>> requeued = new LinkedList<>();
+			try {
+				Iterator<Handler<E>> it = handlers.iterator();
+				Handler<E> handler;
+				while (it.hasNext()) {
+					handler = it.next();
+					if (!handler.tryHandle(data)) {
+						it.remove();
+						requeued.add(handler);
+					}
+				}
+
+				return true;
+			}
+			catch (VetoEventException e) {
+				return false;
+			}
+			catch (ConsumeEventException e) {
+				return true;
+			}
+			finally {
+				handlers.addAll(requeued);
+			}
+		}
+	}
+
 	public interface Ranked<E> extends EventStream.Default<E> {
 		@SuppressWarnings({ "unchecked", "rawtypes" })
 		@Override
 		public default Set<Handler<E>> getHandlers() {
-			return (Set<Handler<E>>) (Set) EventStream.Default.eventStreams.computeIfAbsent(this, k -> new PrioritySet<>());
+			return (Set) EventStream.Default.eventStreams.computeIfAbsent(this, k -> new PrioritySet<>());
 		}
-		
-		
+
+
 		@Override
 		default Handler<? extends E> on(Handler<? extends E> handler) {
 			return this.on(handler, 0);
@@ -79,18 +135,18 @@ public interface EventStream<E> {
 
 		default RankedHandler<? extends E> on(Handler<? extends E> handler, int priority) {
 			RankedHandler<? extends E> rankedHandler = new RankedHandler<>(handler, priority);
-			
+
 			EventStream.Default.super.on(rankedHandler);
-			
+
 			return rankedHandler;
 		}
-		
+
 
 		@Override
 		default boolean off(Handler<? extends E> handler) {
 			return this.off(new RankedHandler<>(handler, 0));
 		}
-		
+
 		default boolean off(RankedHandler<? extends E> handler) {
 			return getHandlers().remove(handler);
 		}
---
+++ src/test/java/aanchev/eventful/OrganizingEventStreamTest.java
@@ -0,0 +1,87 @@
+package aanchev.eventful;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+@Deprecated
+public class OrganizingEventStreamTest {
+
+
+	private static class Elem {
+		private static class A extends Elem {}
+		private static class B extends Elem {}
+	}
+
+
+	protected static class Avg {
+		private double avg = 0;
+		private int cnt = 0;
+
+		public double get() {
+			return avg;
+		}
+
+		public void add(long i) {
+			avg = ((avg * cnt) + i) / (cnt+1);
+			cnt++;
+		}
+	}
+
+
+	@Test
+	public void reportSpeedSame() {
+		reportSpeedComparison(
+				new EventStream.Default<Elem>(){},
+				new EventStream.Default<Elem>(){});
+	}
+
+	@Test
+	public void reportSpeedDiff() {
+		reportSpeedComparison(
+				new EventStream.Default<Elem>(){},
+				new EventStream.Default.Organizing<Elem>(){});
+	}
+
+	public static void reportSpeedComparison(EventStream<Elem> evn0, EventStream<Elem> evn1) {
+		int Nf = 10, Nt = 5;
+		int Nc = 10_000_000;
+
+		List<Handler<? extends Elem>> handlers = new ArrayList<>(Nf+Nt);
+		for (int i=0; i<Nf+Nt; i++)
+			handlers.add(i<Nt? ((Elem.A e) -> act(e)) : ((Elem.B e) -> act(e)));
+		Collections.shuffle(handlers);
+
+		for (Handler<? extends Elem> handler : handlers) {
+			evn0.on(handler);
+			evn1.on(handler);
+		}
+
+
+		long t0;
+		Avg avg0 = new Avg();
+		Avg avg1 = new Avg();
+
+		for (int i=0; i<Nc; i++) {
+			final Elem data = new Elem.A();
+
+			t0 = System.nanoTime();
+			evn0.fire(data);
+			avg0.add(System.nanoTime() - t0);
+
+			t0 = System.nanoTime();
+			evn1.fire(data);
+			avg1.add(System.nanoTime() - t0);
+		}
+
+		System.out.println("Average firing times for "+Nc+" prcs:");
+		System.out.format("\tEventStream 0:  %6f%n", avg0.get());
+		System.out.format("\tEventStream 1:  %6f%n", avg1.get());
+	}
+
+	private static <E> void act(E e) {
+//		System.out.println("Caught fire: " + e);
+	}
+}
